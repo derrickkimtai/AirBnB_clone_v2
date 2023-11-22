8b741df4142c56a3640f10da4dff5f610dc37d6a
Xframex <loukili.issmael@gmail.com>
2023-11-22 11:43:48 -0800

    fixing 2 storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            cls_dict = {}
+
+            for key, value in FileStorage.__objects.items():
+                if cls.__name__ == value.__class__.__name__:
+                    cls_dict[key] = value
+            return cls_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
