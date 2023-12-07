9fc57dae5539c79139e9b4e1246a211067f67ffc
derrickkimtai <derrickmoio92@gmail.com>
2023-12-07 12:30:39 +0300

    airbnb

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-    fabric script that distributes an archive to web servers using webservers using do_deploy function
+    fabric script that distributes an archive to web servers
 """
 from fabric.api import env, run, put
 import os
 
 env.hosts = ['52.87.222.116', '54.209.112.128']
+
 
 def do_deploy(archive_path):
     """
@@ -25,12 +26,17 @@ def do_deploy(archive_path):
 
         put(archive_path, '/tmp/')
         run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, folder_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(file_name, folder_name))
         run('rm /tmp/{}'.format(file_name))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(folder_name, folder_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
+        run('mv /data/web_static/releases/{}/web_static/* '
+            '/data/web_static/releases/{}/'
+            .format(folder_name, folder_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(folder_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(folder_name))
 
         return True
     except Exception as e:
