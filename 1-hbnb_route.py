cd04d6ea9f9b06cfa4a21975e631103275c34d82
derrickkimtai <derrickmoio92@gmail.com>
2023-12-27 15:20:04 +0300

    framework

---
+++ 1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This is a simple Flask web application that listens on 0.0.0.0, port 5000.
+It defines a single route '/' that displays "Hello HBNB!" when accessed.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+   """
+   This function handles requests to the root URL of the server.
+   It returns the string 'Hello HBNB!'.
+   """
+   return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def bnb():
+   """
+   This function handles requests to the root URL of the server.
+   It returns the string HBNB!'.
+   """
+   return 'HBNB'
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=5000)
